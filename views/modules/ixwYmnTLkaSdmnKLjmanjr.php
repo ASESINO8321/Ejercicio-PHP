7d3b1d74ea25f4177deb857186a17095e6636e4b
ASESINO8321 <94855366+ASESINO8321@users.noreply.github.com>
2022-07-15 16:37:02 -0500

    primer commit

--- views/modules/ixwYmnTLkaSdmnKLjmanjr.php
+++ views/modules/ixwYmnTLkaSdmnKLjmanjr.php
@@ -1,1 +1,7 @@
-<h1>inicio</h1>
+<form method="post">
+    <h1>Registrarse</h1>
+    <input type="text" name="name" placeholder="Nombre de usuario">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="contraseña" placeholder="Contraseña">
+    <input type="submit" name="Registrar"> 
+</form>
