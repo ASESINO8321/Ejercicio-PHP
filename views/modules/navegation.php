9cced8f48f79df5d36ab8fc0866f441f9c86898a
ASESINO8321 <94855366+ASESINO8321@users.noreply.github.com>
2022-07-15 09:24:23 -0500

    primer commit

--- views/modules/navegation.php
+++ views/modules/navegation.php
@@ -1,9 +1,10 @@
 <nav class="nav">
+    <a href="#" class="logo">Logotipo</a>
     <ul class="navUl">
-        <li class="navLi"><a href="#"></a></li>
-        <li class="navLi"><a href="#"></a></li>
-        <li class="navLi"><a href="#"></a></li>
-        <li class="navLi"><a href="#"></a></li>
-        <li class="navLi"><a href="#"></a></li>
+        <li class="navLi"><a href="index.php?v=ixwYmnTLkaSdmnKLjmanjr">Inicio</a></li>
+        <li class="navLi"><a href="index.php?v=rHnfgyhuKJeghmjkdHFGBT">Registrarse</a></li>
+        <li class="navLi"><a href="index.php?v=dOJHGwrwvnppitbDGBDTNJ">Dibujar</a></li>
+        <li class="navLi"><a href="index.php?v=AchgikghfJGJGUYGBJgyug">Acerca de</a></li>
+        <li class="navLi"><a href="index.php?v=cNGHdfhrwrgvJHGHTFXjby">Contactenos</a></li>
     </ul>
 </nav>
